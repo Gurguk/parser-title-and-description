e50ff72ce59a143252770aa40e6c802363c6a2cb
Roman Schastlyvets <poweroid89@gmail.com>
2020-02-07 15:58:58 +0200

    API version 2

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var options = {
 var app = express();
 
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', function(req, res) {
     res.send('API for extraction titles');
@@ -26,7 +28,7 @@ app.post('/api/v1/find-urls', (req, res) => {
 });
 
 app.post('/api/v1/find-urls2', (req, res) => {
-    res.status(200).send(req.params)
+    res.status(200).send(req.body)
 });
 
 app.post('/api/v1/extract-data', (req, res) => {
